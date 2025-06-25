77335bc0f035f74c9d48b7aa30f5a1d7fb2817e3
mohd kaif <kaifkhhan1040@gmail.com>
2025-06-25 07:43:09 +0000

    up

--- website/views.py
+++ website/views.py
@@ -32,25 +32,25 @@ def tire(request):
     return render(request,'web/tire.html',{'car_brands_grouped':car_brands_grouped})
 
 
-def load_exchange_rates(file_path='exchange_rates.json'):
-    with open(file_path, 'r') as f:
-        return json.load(f)
+# def load_exchange_rates(file_path='exchange_rates.json'):
+#     with open(file_path, 'r') as f:
+#         return json.load(f)
 
-def convert_currency(amount_in_inr, to_currency, rates):
-    rate = rates.get(to_currency.upper())
-    if rate:
-        return amount_in_inr * rate
-    else:
-        return "Currency not supported."
+# def convert_currency(amount_in_inr, to_currency, rates):
+#     rate = rates.get(to_currency.upper())
+#     if rate:
+#         return amount_in_inr * rate
+#     else:
+#         return "Currency not supported."
 
-# Load rates from JSON
-rates = load_exchange_rates()
+# # Load rates from JSON
+# rates = load_exchange_rates()
 
-# Convert example
-amount = 1000
-currency = 'EUR'
-converted = convert_currency(amount, currency, rates)
-print(f"{amount} INR = {converted:.2f} {currency}")
+# # Convert example
+# amount = 1000
+# currency = 'EUR'
+# converted = convert_currency(amount, currency, rates)
+# print(f"{amount} INR = {converted:.2f} {currency}")
 
 
 
